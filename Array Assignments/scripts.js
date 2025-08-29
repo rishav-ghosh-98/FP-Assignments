d610e5de8d10485fc6301a8ccefe33ca9bd3c32b
Rishav Ghosh <rishavarighs@gmail.com>
2025-08-30 05:07:08 +0530

    addes some more problems

--- Array Assignments/scripts.js
+++ Array Assignments/scripts.js
@@ -51,3 +51,123 @@ const pets = [
 //   dog: [ { name: 'Spot', type: 'dog' }, { name: 'Rover', type: 'dog' } ],
 //   cat: [ { name: 'Mittens', type: 'cat' }, { name: 'Whiskers', type: 'cat' } ]
 // }
+
+const filterPets = pets.reduce(
+  (acc, curr) => {
+    !acc[curr.type] ? acc[curr.type] = [] :  acc[curr.type].push(curr);
+  return acc
+},
+  [{}]
+);
+console.log(filterPets)
+
+const products = [
+  { sku: 'A123', name: 'Socks', price: 5 },
+  { sku: 'B456', name: 'Hat', price: 15 }
+];
+
+// expected result:
+// {
+//   A123: { sku: 'A123', name: 'Socks', price: 5 },
+//   B456: { sku: 'B456', name: 'Hat', price: 15 }
+// }
+
+const filterProducts = products.reduce((acc,curr) => {
+    acc[curr.sku] = curr
+    return acc
+},{})
+console.log(filterProducts)
+
+const votes = [
+  'Alice', 'Bob', 'Alice', 'Carol',
+  'Bob', 'Alice', 'Carol', 'Bob'
+];
+// expected result:
+// { Alice: 3, Bob: 3, Carol: 2 }
+
+const countVotes = votes.reduce((acc,curr) => {acc[curr] = (acc[curr] || 0)+1
+    return acc;
+},{})
+console.log(countVotes)
+
+const pets1  = [
+  { name: 'Fido', type: 'dog' },
+  { name: 'Mittens', type: 'cat' },
+  { name: 'Rex', type: 'dog' },
+  { name: 'Whiskers', type: 'cat' },
+  { name: 'Goldie', type: 'fish' }
+];
+
+// expected result:
+// { dog: 2, cat: 2, fish: 1 }
+
+const filterPetsTypes = pets1.reduce((acc,curr)=> {
+    acc[curr.type]  = (acc[curr.type] || 0) + 1;
+    return acc;
+},{})
+console.log(filterPetsTypes)
+
+const sales = [
+	{ region: 'North', amount: 100 },
+	{ region: 'South', amount:  80 },
+	{ region: 'North', amount: 120 },
+	{ region: 'East',  amount:  75 },
+	{ region: 'South', amount:  20 }
+]
+
+// expected result:
+// { North: 220, South: 100, East: 75 }
+const filterSales = sales.reduce((acc,curr) => {
+    acc[curr.region] = (acc[curr.region] || 0) + curr.amount;
+    return acc;
+},{})
+console.log(filterSales)
+
+const str =  'mississippi';
+// expected result:
+// { m: 1, i: 4, s: 4, p: 2 }|
+let obj ={}
+for(let i = 0; i<str.length;i++){
+    let char = str[i]
+    if (obj[char]){
+        obj[char] += 1;
+    }else{
+        obj[char] = 1;
+    }
+}
+console.log(obj)
+const filterChar = str.split('').reduce((acc,curr) => {
+  acc[curr] = (acc[curr]  || 0)+ 1  ;
+  return acc;
+},{})
+console.log(filterChar)
+
+const users1 = [
+  { name: 'Anjali', active: true },
+  { name: 'Raj', active: false },
+  { name: 'Meena', active: true }
+];
+
+// Expected Output
+["Anjali", "Meena"]
+
+const activeUsers = users1.reduce((acc,curr) => {curr.active = true ? acc.push(curr.name) : acc;
+    return acc
+},[]);
+console.log(activeUsers)
+
+const participation = [
+  { name: 'Rahul', hours: 6 },
+  { name: 'Neha', hours: 4 },
+  { name: 'Ankit', hours: 3 },
+  { name: 'Rahul', hours: 5 },
+  { name: 'Neha', hours: 2 },
+  { name: 'Anjali', hours: 9 }
+];
+
+// Expected Output:
+// {
+//   Rahul: 11,
+//   Anjali: 9
+// }
+
